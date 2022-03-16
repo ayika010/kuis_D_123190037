864d156188af89b41720250a902b1a73d8de2de2
unknown <starkgement@gmail.com>
2022-03-16 12:22:01 +0700

    123190037

---
+++ lib/detail_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_application_kuis/app_store.dart';
+
+class DetailPage extends StatelessWidget {
+  final AppStore place;
+
+  DetailPage({required this.place});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
